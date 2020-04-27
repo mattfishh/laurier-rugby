fbb3c05d73be80ae7fcb0cf58e715556c3de8641
chorner98 <chorner@Charlies-MacBook-Pro.local>
2020-04-27 16:17:49 -0400

    Created a slideshow on the homepage

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import './components/Home.css'
+import SlideShowImage from './components/SlideShowImage';
 
 export const Home = () => (
-    <div>
-        <h2> future site of homepage</h2>
+    <div class='siteBody'>
+    	<SlideShowImage />
+
+    	<div class='previousScoreDiv'>
+    		<h4>Most Recent Score Placeholder</h4>
+    	</div>
     </div>
 )
--- src/NavBar.js
+++ src/NavBar.js
@@ -13,33 +13,34 @@ function NavBar() {
   var page = arr.pop();
 
   return (
-  <div class='componentModel'>
-    <div class='navHeader'>
-      <h4 class='navHeaderText'>LAURIER RUGBY</h4>
-      <img class='navBarImg' src="https://www.pngkey.com/png/full/67-674639_wlu-laurier-golden-hawks-logo-no-text-wilfrid.png"/>
+    <div class='componentModel'>
+      <div class='navHeader'>
+        <h4 class='navHeaderText'>LAURIER RUGBY</h4>
+        <img class='navBarImg' src="https://www.pngkey.com/png/full/67-674639_wlu-laurier-golden-hawks-logo-no-text-wilfrid.png"/>
+      </div>
+      
+      <div class='navBarItems'>
+          <Navbar variant="dark" id="nav" expand="lg">
+            <Navbar.Toggle aria-controls="basic-navbar-nav " />
+            <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/schedule">Schedule</Nav.Link>
+              <Nav.Link href="/alumni">Alumni</Nav.Link>
+              <Nav.Link href="/recruits">Recruits</Nav.Link>
+              <Nav.Link href="/roster">Roster</Nav.Link>
+              <Nav.Link href="/media">Media</Nav.Link>
+              <Nav.Link href="/sponsors">Corporate Sponsors</Nav.Link>
+              <NavDropdown title="Other" id="basic-nav-dropdown">
+                <NavDropdown.Item href="/contact">Contact Us</NavDropdown.Item>
+                <NavDropdown.Item href="/coaches">Coaching Staff</NavDropdown.Item>
+                <NavDropdown.Item href="/forgotsomething">Forgot Something Probably</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
     </div>
-    <div class='navBarItems'>
-        <Navbar variant="dark" id="nav" expand="lg">
-          <Navbar.Toggle aria-controls="basic-navbar-nav " />
-          <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/schedule">Schedule</Nav.Link>
-            <Nav.Link href="/alumni">Alumni</Nav.Link>
-            <Nav.Link href="/recruits">Recruits</Nav.Link>
-            <Nav.Link href="/roster">Roster</Nav.Link>
-            <Nav.Link href="/media">Media</Nav.Link>
-            <Nav.Link href="/sponsors">Corporate Sponsors</Nav.Link>
-            <NavDropdown title="Other" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/contact">Contact Us</NavDropdown.Item>
-              <NavDropdown.Item href="/coaches">Coaching Staff</NavDropdown.Item>
-              <NavDropdown.Item href="/forgotsomething">Forgot Something Probably</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </div>
-  </div>
   );
 }
 
