c52b054c8094f65e7dde4a4ee5beeb2199c217c5
russellgoldman <russellgoldman1@gmail.com>
2020-04-28 17:43:00 -0400

    removed console log

--- src/ApolloClient.js
+++ src/ApolloClient.js
@@ -1,7 +1,5 @@
 import ApolloClient from 'apollo-boost';
 
-console.log(process.env.REACT_APP_PRODUCTION_GRAPHQL_ENDPOINT)
-
 const client = new ApolloClient({
   uri: process.env.REACT_APP_PRODUCTION_GRAPHQL_ENDPOINT,
 });
